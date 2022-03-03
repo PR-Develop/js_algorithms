d4617312eadb85341647c28bf15a8f626a665dc1
lucaspalomodevelop <lucas.palomo@t-online.de>
2022-03-03 19:21:06 +0100

    add asc/desc

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
 let bubblesort = require("./bubblesort");
+let dataGenerator = require("./dataGenerator");
 
 console.log(bubblesort([1, 3, 2], true));
+
+console.log(dataGenerator.generateUnsortedIntArray(10));
